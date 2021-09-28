3f7704df43cea94781c72dedfeed20209cf49e9e
Ali Hassan <Alideveloper95@gmail.com>
2021-09-28 18:58:41 +0500

    Custom data type

--- hashed_block/src/main.rs
+++ hashed_block/src/main.rs
@@ -1,12 +1,21 @@
+
+
 // create new module name block_node which provide blockchian like functionalities at basic level. 
 // Blockchain is very complex topic. However Pub because this crate accessible in main module
 pub mod block_node{
-    
+    #[derive(Debug)]
     // create Block data struct
     pub struct Block{
         pub id : String,
         pub name : Vec<String>,
         pub tx : i64,
+    }
+
+    // more custom type block
+    #[derive(Debug)]
+    pub enum Results{
+        OK(Block,u64),
+        Error(Block)
     }
 
     // importing packages that require for this module
@@ -16,33 +25,40 @@ pub mod block_node{
     // Impl allow to Block functionalities which is only available for Block structure.
     impl Block{
 
-        pub fn create( self) -> u64{
+        pub fn create( self) -> Results{
             
             // Default_Hasher allow us to use hash :: crate 
             let mut hasher = DefaultHasher::new();
             
-            // User can't provide data then no block created
+            // block created only when user provided his name
             if self.name.get(0) == Some(&" ".to_string()){
-               return 0
-            }
+                // in this case block boardcast block information
+                let result:Results = Results::Error(self);
+                return result
+             }
 
-            // If block contains user name then take hash and return rust return hash (u64) but in future. I will conversion for string too, 
-            Hash::hash_slice(&self.name, &mut hasher);
-            let block_hash : u64 = hasher.finish();
-             return block_hash
+            //  build hash of block::Name 
+             Hash::hash_slice(&self.name, &mut hasher);
+             let block_hash : u64 = hasher.finish();
+             
+            //  Block boardcast block information along with hash of Block::Name Field
+             let result = Results::OK(self, block_hash);
+             return result
         }
     }
 }
 
+// import crate
 use crate::block_node::Block;
+use crate::block_node::Results;
 fn main() {
     
     let mut name : Vec<String> = Vec::new();
     name.push("Ali".to_string());
     let id = "0".to_string();
     let block = Block{id , name, tx: 0};
-    let data : u64 = block.create();
-    println!(" block create : ({})", data );
+    let data : Results = block.create();
+    println!(" block create : ({:#?})", data );
     
 }
 
---
+++ hashed_block/target/debug/deps/hashed_block-38a265a66caa6541.d
@@ -0,0 +1,5 @@
+/home/ali/Documents/turbo-invention/hashed_block/target/debug/deps/hashed_block-38a265a66caa6541: src/main.rs
+
+/home/ali/Documents/turbo-invention/hashed_block/target/debug/deps/hashed_block-38a265a66caa6541.d: src/main.rs
+
+src/main.rs:
